73ad7e116995e958f11e92c2e19390693d596284
118140126 Muhammad Ichsan Ramadhan <muhammad.118140126@student.itera.ac.id>
2020-11-18 04:33:51 +0700

    Ini Koneksi DB Tugas 3 MySQLi

---
+++ Minggu-3/koneksi.php
@@ -0,0 +1,10 @@
+	<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "pemweb";
+	$koneksi = mysqli_connect("$server","$username","$password","$database");
+	if (mysqli_connect_errno()){
+		echo "Koneksi Database Gagal : " . mysqli_connect_error();
+	}
+	?>
